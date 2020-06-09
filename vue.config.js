19253d7aae74b4b96fb00013347549fbc7018c06
Renzo Carara <renzo.carara@libero.it>
2020-06-09 23:54:42 +0200

    prove di deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     // transpileDependencies: ["vuetify"],
-    transpileDependencies: [/node_modules[/\\\\]vuetify[/\\\\]/],
+    transpileDependencies: ["\\vuetify\\"],
     publicPath: "/vuetify-clock/",
 };
