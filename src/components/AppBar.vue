e6477d2ff4f1f8936d2a39c87c332cd1b7e0c1da
Renzo Carara <renzo.carara@libero.it>
2020-06-09 21:47:32 +0200

    modificata favicon, logo, e aggiunta immagine per thumbnail di opengraph (meta og:image)

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -6,14 +6,16 @@
         height="56"
         >
         <div class="d-flex align-center flex-grow-1 justify-space-between">
-            <v-img
-            alt="uk flag"
-            class="shrink mr-4 img-rounded"
-            contain
-            src="@/assets/en.svg"
-            transition="scale-transition"
-            width="40"
-            />
+            <router-link to="/">
+                <v-img
+                alt="uk flag"
+                class="shrink mr-4"
+                contain
+                src="@/assets/vuetify_logo.svg"
+                transition="scale-transition"
+                width="40"
+                />
+            </router-link>
 
             <h1>Vuetify-Clock</h1>
 
@@ -26,18 +28,4 @@
 
 <style lang="scss" scoped>
 @import "@/sass/app.scss";
-
-// questa puo' servire nella AppBar style
-#nav {
-    padding: 30px;
-
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-
-        &.router-link-exact-active {
-            color: #42b983;
-        }
-    }
-}
 </style>
